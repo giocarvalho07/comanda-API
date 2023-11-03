5ce8b01c40437d0c3997ccf485884474af882d89
giocarvalho07 <giovannidecarvalhomendes@outlook.com>
2023-11-02 21:05:24 -0300

    adicionar cors na api

--- src/main/java/com/comandaai/controller/DashboardController.java
+++ src/main/java/com/comandaai/controller/DashboardController.java
@@ -7,6 +7,7 @@ import com.comandaai.repository.ProdutoRepository;
 import com.comandaai.service.PedidoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,7 +19,8 @@ import java.util.Map;
 
 
 @RestController
-@RequestMapping("api/v1/dashboard")
+@CrossOrigin(origins = "https://comanda-api.onrender.com") // Substitua pelo domínio do seu site
+@RequestMapping("/api/v1/dashboard")
 public class DashboardController {
 
     // Injeção de dependência do repositório de pedidos
